25c4e587c9f934305e573b51ce45a25b8fddb5fc
P3d11 <pee.marchi@hotmail.com>
2022-11-01 14:57:38 -0300

    hello world :)

---
+++ hello_world/hello_world/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float milisec = 2000.0f;
+            int milisec_int = Convert.ToInt32(milisec);
+
+            Console.WriteLine("hello world {0}", milisec);
+
+            Thread.Sleep(milisec_int);
+        }
+    }
+}
